dd83355fd1757e010494d955d117d658bc2921f7
Quinntyne Brown <quinntynebrown@gmail.com>
2017-10-10 19:29:19 -0400

    initial-commit

---
+++ dist/main.d.ts
@@ -0,0 +1,4 @@
+export declare class HelloWorldComponent extends HTMLElement {
+    constructor();
+    connectedCallback(): void;
+}
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+﻿import { html, render, TemplateResult } from "lit-html";
+
+
+export class HelloWorldComponent extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+    }
+    connectedCallback() {
+        render(html``, this.shadowRoot);
+    }
+}
+
+
+customElements.define("hello-world", HelloWorldComponent);
