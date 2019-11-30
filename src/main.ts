827aae45e1890e9d67105a9652f6ececa9965070
Quinntyne Brown <Quinntyne.Brown@trader.ca>
2019-11-29 21:17:01 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -1,81 +1,66 @@
-﻿class RibbonEvents {
-    public static readonly reset = "RESET";
-    public static readonly translate = "TRANSLATE";
-}
+﻿class Strip {
 
-class CustomEventDispatcher {
-    static dispatch(element:HTMLElement, type:string, payload = null) {
-        element.dispatchEvent(new CustomEvent(type,{ bubbles: true, detail: payload }))
-    }
-}
-
-class Ribbon {
-
-    constructor(element: HTMLElement, $document: Document, $window: Window) {
+    constructor(element: HTMLElement) {
         this._element = element;
-        this._document = $document;
-        this._window = $window;
-        this._registerEventListeners();
-        CustomEventDispatcher.dispatch(this._element, RibbonEvents.reset);
-    }
-
-    private _registerEventListeners() {
         this._hammerManager = new Hammer(this._element);
         this._hammerManager.get('pan').set({ direction: Hammer.DIRECTION_HORIZONTAL });
         this._hammerManager.on("panmove",this._handlePanMove);
-        this._document.addEventListener(RibbonEvents.translate,this._handleTranslate);
-        this._window.addEventListener("resize", this._handleResize);
-        this._document.addEventListener(RibbonEvents.reset, this._handleReset);
     }
-    
-    private _handleTranslate = (e: CustomEvent) => {
-        
-        if(this._element.offsetWidth >= 1170) return false;
-        
-        const newDeltax =  this._deltaX + e.detail.deltaX;
 
-        if(newDeltax >  this._element.offsetWidth - this._body.offsetWidth) {            
-            this._deltaX = newDeltax;            
-        } else {
-            this._deltaX = this._element.offsetWidth - this._body.offsetWidth;
+    private _handlePanMove = (e) => {
+
+        if (this._element.offsetWidth >= 1170) return false;
+
+        this._deltaX = this._deltaX + e.deltaX;
+
+        if(this._deltaX > this._max) {
+            this._previousButton.classList.remove(this._disabledCssClass)                
         }
-    
-        if(newDeltax > 0)          
-            this._deltaX = 0;            
-        
+
+        if(this._deltaX < this._min) {
+            this._nextButton.classList.remove(this._disabledCssClass)                
+        }
+
+
+        if (this._deltaX <= this._max) {
+            this._deltaX = this._max;   
+            
+            if(!this._nextButton.classList.contains(this._disabledCssClass))
+                this._nextButton.classList.add(this._disabledCssClass)
+        }
+
+        if(this._deltaX  > this._min) {
+            this._deltaX = this._min;    
+            
+            if(!this._previousButton.classList.contains(this._disabledCssClass))
+                this._previousButton.classList.add(this._disabledCssClass)            
+        }
+
         this._body.style.transform = `translateX(${this._deltaX}px)`;
-    }
 
-    private _handlePanMove = (e) => {            
-        CustomEventDispatcher.dispatch(this._element, RibbonEvents.translate, { deltaX: e.deltaX });
-    }
-
-    private _handleReset = (e) => {
-        this._deltaX = 0;
-        this._body.style.transform = `translateX(${this._deltaX}px)`;
-    }
-
-    private _handleResize = (e) => {
-        CustomEventDispatcher.dispatch(this._element, RibbonEvents.reset);
+        return true;
     }
 
     static mount(element:HTMLElement) {
-        var elements = element.querySelectorAll(".ribbon") as NodeListOf<HTMLElement>;
+        var elements = element.querySelectorAll(".strip") as NodeListOf<HTMLElement>;
 
         for(var i = 0; i < elements.length; i++) {
-            new Ribbon(elements[i], document,window);
+            new Strip(elements[i]);
         }
     }
 
     private _deltaX = 0;
     private _element: HTMLElement;
     private _hammerManager: HammerManager;
-    private get _body(): HTMLElement { return this._element.querySelector(".ribbon__body") as HTMLElement; };
-    private _document: Document;
-    private _window: Window;
+    private get _body(): HTMLElement { return this._element.querySelector("ul"); };
+    private get _nextButton(): HTMLElement { return this._element.querySelector(".strip--next") as HTMLElement; }
+    private get _previousButton(): HTMLElement { return this._element.querySelector(".strip--previous") as HTMLElement; }
+    private get _max():number { return this._element.offsetWidth - this._body.offsetWidth; }
+    private get _min() { return 0; }
+    private _disabledCssClass = "strip--button-disabled";
 }
 
 document.addEventListener("readystatechange",() => {
     if(document.readyState == "complete")
-        Ribbon.mount(document.querySelector("body"));    
+        Strip.mount(document.querySelector("body"));    
 })
